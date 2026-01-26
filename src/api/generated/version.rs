57319428ba2b454e95eb293cd49f32d50cc42131
David Koller <david.koller@xmv.de>
2026-01-26 17:53:48 +0100

    fix: improve code formatting and readability in version service

--- src/api/generated/version.rs
+++ src/api/generated/version.rs
@@ -20,10 +20,10 @@ pub mod version_service_client {
         dead_code,
         missing_docs,
         clippy::wildcard_imports,
-        clippy::let_unit_value
+        clippy::let_unit_value,
     )]
-    use tonic::codegen::http::Uri;
     use tonic::codegen::*;
+    use tonic::codegen::http::Uri;
     #[derive(Debug, Clone)]
     pub struct VersionServiceClient<T> {
         inner: tonic::client::Grpc<T>,
@@ -67,8 +67,9 @@ pub mod version_service_client {
                     <T as tonic::client::GrpcService<tonic::body::BoxBody>>::ResponseBody,
                 >,
             >,
-            <T as tonic::codegen::Service<http::Request<tonic::body::BoxBody>>>::Error:
-                Into<StdError> + std::marker::Send + std::marker::Sync,
+            <T as tonic::codegen::Service<
+                http::Request<tonic::body::BoxBody>,
+            >>::Error: Into<StdError> + std::marker::Send + std::marker::Sync,
         {
             VersionServiceClient::new(InterceptedService::new(inner, interceptor))
         }
@@ -106,12 +107,22 @@ pub mod version_service_client {
         pub async fn version(
             &mut self,
             request: impl tonic::IntoRequest<super::VersionRequest>,
-        ) -> std::result::Result<tonic::Response<super::VersionResponse>, tonic::Status> {
-            self.inner.ready().await.map_err(|e| {
-                tonic::Status::unknown(format!("Service was not ready: {}", e.into()))
-            })?;
+        ) -> std::result::Result<
+            tonic::Response<super::VersionResponse>,
+            tonic::Status,
+        > {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::unknown(
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
             let codec = tonic::codec::ProstCodec::default();
-            let path = http::uri::PathAndQuery::from_static("/version.VersionService/Version");
+            let path = http::uri::PathAndQuery::from_static(
+                "/version.VersionService/Version",
+            );
             let mut req = request.into_request();
             req.extensions_mut()
                 .insert(GrpcMethod::new("version.VersionService", "Version"));
@@ -126,7 +137,7 @@ pub mod version_service_server {
         dead_code,
         missing_docs,
         clippy::wildcard_imports,
-        clippy::let_unit_value
+        clippy::let_unit_value,
     )]
     use tonic::codegen::*;
     /// Generated trait containing gRPC methods that should be implemented for use with VersionServiceServer.
@@ -158,7 +169,10 @@ pub mod version_service_server {
                 max_encoding_message_size: None,
             }
         }
-        pub fn with_interceptor<F>(inner: T, interceptor: F) -> InterceptedService<Self, F>
+        pub fn with_interceptor<F>(
+            inner: T,
+            interceptor: F,
+        ) -> InterceptedService<Self, F>
         where
             F: tonic::service::Interceptor,
         {
@@ -213,9 +227,15 @@ pub mod version_service_server {
                 "/version.VersionService/Version" => {
                     #[allow(non_camel_case_types)]
                     struct VersionSvc<T: VersionService>(pub Arc<T>);
-                    impl<T: VersionService> tonic::server::UnaryService<super::VersionRequest> for VersionSvc<T> {
+                    impl<
+                        T: VersionService,
+                    > tonic::server::UnaryService<super::VersionRequest>
+                    for VersionSvc<T> {
                         type Response = super::VersionResponse;
-                        type Future = BoxFuture<tonic::Response<Self::Response>, tonic::Status>;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::Response>,
+                            tonic::Status,
+                        >;
                         fn call(
                             &mut self,
                             request: tonic::Request<super::VersionRequest>,
@@ -249,19 +269,23 @@ pub mod version_service_server {
                     };
                     Box::pin(fut)
                 }
-                _ => Box::pin(async move {
-                    let mut response = http::Response::new(empty_body());
-                    let headers = response.headers_mut();
-                    headers.insert(
-                        tonic::Status::GRPC_STATUS,
-                        (tonic::Code::Unimplemented as i32).into(),
-                    );
-                    headers.insert(
-                        http::header::CONTENT_TYPE,
-                        tonic::metadata::GRPC_CONTENT_TYPE,
-                    );
-                    Ok(response)
-                }),
+                _ => {
+                    Box::pin(async move {
+                        let mut response = http::Response::new(empty_body());
+                        let headers = response.headers_mut();
+                        headers
+                            .insert(
+                                tonic::Status::GRPC_STATUS,
+                                (tonic::Code::Unimplemented as i32).into(),
+                            );
+                        headers
+                            .insert(
+                                http::header::CONTENT_TYPE,
+                                tonic::metadata::GRPC_CONTENT_TYPE,
+                            );
+                        Ok(response)
+                    })
+                }
             }
         }
     }
